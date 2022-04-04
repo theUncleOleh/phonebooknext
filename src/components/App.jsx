5dd8872e9e320cdfc153152f450c719dcc1ce253
Oleh Semennykov <sumcue@gmail.com>
2022-04-04 14:59:26 +0300

    first

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,33 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        textTransform: 'uppercase',
-        color: '#010101',
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import { nanoid } from 'nanoid';
+import Form from './Form/Form';
+
+class App extends Component {
+  state = {
+    contacts: [],
+  };
+
+  formSubmitHandler = data => {
+    console.log(data);
+  };
+  // handleNameChange = event => {
+  //   console.log(event.currentTarget.value);
+  //   this.setState({ name: event.currentTarget.value });
+  // };
+  // handleNumberChange = event => {
+  //   this.setState({ number: event.currentTarget.value });
+  // };
+
+  render() {
+    return (
+      <div>
+        <Form onSubmit={this.formSubmitHandler} />
+        <ul>
+          <li></li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default App;
