793919804b2bbaae725b9e6afe903678a11cfc9b
Oleh Semennykov <sumcue@gmail.com>
2022-04-08 17:11:14 +0300

    add formik

---
+++ src/components/Formik/FormByFormik.jsx
@@ -0,0 +1,65 @@
+import { Formik } from 'formik';
+import * as Yup from 'yup';
+import React, { Component } from 'react';
+
+export default function FormByFormik() {
+  return (
+    <div>
+      <h1>Phonebook</h1>
+      <Formik
+        initialValues={{ name: '', number: '' }}
+        validationSchema={Yup.object()}
+        // validate={values => {
+        //   const errors = {};
+        //   if (!values.email) {
+        //     errors.email = 'Required';
+        //   } else if (
+        //     !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+        //   ) {
+        //     errors.email = 'Invalid email address';
+        //   }
+        //   return errors;
+        // }}
+        onSubmit={(values, { setSubmitting }) => {
+          setTimeout(() => {
+            alert(JSON.stringify(values, null, 2));
+            setSubmitting(false);
+          }, 400);
+        }}
+      >
+        {({
+          values,
+          errors,
+          touched,
+          handleChange,
+          handleBlur,
+          handleSubmit,
+          isSubmitting,
+          /* and other goodies */
+        }) => (
+          <form onSubmit={handleSubmit} autoComplete="off">
+            <input
+              type="text"
+              name="name"
+              onChange={handleChange}
+              onBlur={handleBlur}
+              value={values.name}
+            />
+            {errors.name && touched.name && errors.name}
+            <input
+              type="number"
+              name="number"
+              onChange={handleChange}
+              onBlur={handleBlur}
+              value={values.number}
+            />
+            {errors.number && touched.number && errors.number}
+            <button type="submit" disabled={isSubmitting}>
+              Submit
+            </button>
+          </form>
+        )}
+      </Formik>
+    </div>
+  );
+}
